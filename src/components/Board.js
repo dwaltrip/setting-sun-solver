ddf4a9461d61a112f06fab7df9be4c83ee24dde6
Daniel Waltrip <dwaltrip77@gmail.com>
2021-08-20 16:00:04 -0700

    Playback UI - add colors for pieces

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,3 +1,18 @@
+import {
+  P_1X1_SQUARE,
+  P_2X1_HORIZ,
+  P_2X1_VERT,
+  P_2X2_SQUARE,
+} from "../solver/symbols";
+
+// TODO: Come up with some abstraction for this?
+// How should it work for other puzzles besides the setting sun?
+const BGColors = {
+  [P_1X1_SQUARE]: 'green',
+  [P_2X1_HORIZ]: 'white',
+  [P_2X1_VERT]: 'white',
+  [P_2X2_SQUARE]: 'red',
+};
 
 const UNIT_SIZE = 60;
 const MARGIN = 15;
@@ -46,6 +61,14 @@ function Board({ pieces, rows, cols }) {
               }}
               key={`${type.name} -- ${index}`}
             >
+              <div
+                className='bg-color'
+                style={{
+                  height: `${0.8 * UNIT_SIZE}px`,
+                  width: `${0.8 * UNIT_SIZE}px`,
+                  backgroundColor: BGColors[type.name],
+                }}
+              />
             </div>
           );
         })}
